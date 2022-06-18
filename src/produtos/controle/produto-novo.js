8e53e2035a5b1c00033daf02ec9db836b0b98c4f
KaikSouza <kaiksouza2020@gmail.com>
2022-06-18 12:49:05 -0300

    teste

--- src/produtos/controle/produto-novo.js
+++ src/produtos/controle/produto-novo.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $('.novo-produto').click(function(e) {
+    $('.nome').click(function(e) {
         e.preventDefault()
 
         //Iremos incluir uma função no botão slavar para demonstrar que é um novo registro

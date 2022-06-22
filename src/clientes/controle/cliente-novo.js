da5276e911dcb05efaa040c425f62598c8fd66e6
KaikSouza <kaiksouza2020@gmail.com>
2022-06-22 10:20:16 -0300

    alteração

--- src/clientes/controle/cliente-novo.js
+++ src/clientes/controle/cliente-novo.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
-    //Iremos incluir uma função no botão slavar para demonstrar que é um novo registro
-    $('.novo-produto').attr('data-operation', 'insert')
+    $('.btn-novo-cliente').click(function(e){
+
+        e.preventDefault()
+
+        $('#content').empty()
+        $('#content').load('src/visao/clientes/cliente-cadastrar.html')
+
+        //Iremos incluir uma função no botão slavar para demonstrar que é um novo registro
+        $('.novo-produto').attr('data-operation', 'insert')
+    })
 
 })

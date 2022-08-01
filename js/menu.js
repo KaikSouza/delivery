d39046dd7b023247b9c09d885388ed1bd0785645
KaikSouza <kaiksouza2020@gmail.com>
2022-08-01 10:33:26 -0300

    alteração

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 
-    $('.nav-link').click(function(e) {
-        e.preventDefault()
+    $('.linknav').click(function(nav) {
+        nav.preventDefault()
+
+        let urlnav = $(this).attr('href')
+
+        $('#content').empty()
+
+        $('#content').load(urlnav)
+    })
+
+    $('.dropdown-item').click(function(dropdown) {
+        dropdown.preventDefault()
 
         let url = $(this).attr('href')
 

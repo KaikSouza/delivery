29298c09bce5c31b10278f2b6077eb2a0199d5a5
KaikSouza <kaiksouza2020@gmail.com>
2022-08-05 11:23:41 -0300

    alteração

---
+++ src/clientes/controle/cliente-listar.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    $('#tabela-cliente').DataTable({
+        serverSide: true,
+        scrollY: '40vh',
+        scrollCollapse: true,
+        paging: false,
+        responsive: true,
+        buttons: [
+            'pdf'
+        ],
+        "language":{
+            "url": "libs/DataTables/pt_br.json"
+        },
+        "ajax": {
+            "url": "src/clientes/modelo/cliente-listar.php",
+            "type": "POST"
+        },
+        "columns": [{
+            "data": 'idcliente',
+            "className": 'text-center'
+        },
+        {
+            "data": 'nome',
+            "className": 'text-center'
+        },
+        {
+            "data": 'celular',
+            "className": 'text-center'
+        },
+        {
+            "data": 'email',
+            "className": 'text-center'
+        },
+        {
+            "data": 'idcliente',
+            "className": 'text-center',
+            "orderable": false,
+            "searchable": false,
+            "render": function(data, type, row, meta){
+                return `
+                <button id ="${data}" class = "btn btn-success cliente-pedido-novo"><i class="fa-solid fa-plus"></i></button>
+                `
+            }
+        },
+        {
+            "data": 'idcliente',
+            "className": 'text-center',
+            "orderable": false,
+            "searchable": false,
+            "render": function(data, type, row, meta){
+                return `
+                <button id ="${data}" class = "btn btn-info cliente-editar"><i class="fa-solid fa-pen"></i></button>
+                `
+            }
+        },
+        {
+            "data": 'idcliente',
+            "className": 'text-center',
+            "orderable": false,
+            "searchable": false,
+            "render": function(data, type, row, meta){
+                return `
+                <button id ="${data}" class = "btn btn-danger cliente-excluir"><i class="fa-solid fa-trash"></i></button>
+                `
+            }
+        }
+    ]
+    })
+})
